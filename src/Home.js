5f00df416ba7b247f6c934bdffa3295628caaf20
keegan <hengquan0204@gmail.com>
2023-11-13 19:26:14 +0800

    added Message & changed background colour

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,15 @@ import './Home.css'; // Import your CSS file for Home component
 function Home() {
   return (
     <div className="centered">
-      <h1>Happy 3rd Anniversary bb!</h1>
+      <h1>Happy 3rd Anniversary!</h1>
+	  <h1>I love you bb &lt;3</h1>
       <div className="nav-links">
         <Link to="/message" className="nav-link">
-          Go to Message
+          Anniversary Message
         </Link>
         {' | '}
         <Link to="/quiz" className="nav-link">
-          Go to Quiz
+          Anniversary Quiz
         </Link>
       </div>
     </div>
--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,20 @@
+// Message.js
+
 import React from 'react';
+import './Message.css';
 
 function Message() {
   return (
     <div className="centered">
       <h2>Happy Anniversary my bb kitty :)</h2>
-      <p>Bello bb, we are 3! I love you!</p>
+      <p>
+        Bello bb, we are 3!
+        <br />
+		I am deeply grateful for the love and care you provided me for these <br />3 years. Without you, I don't think I would be able to survive this harsh<br /> early adulting period. So, thank you bb!
+      </p>
     </div>
   );
 }
 
 export default Message;
+
