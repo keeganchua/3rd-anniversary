0121468035ba73aac8384f8fcfe1002619a112ba
keegan <hengquan0204@gmail.com>
2023-11-13 19:47:18 +0800

    align Message

--- src/Message.js
+++ src/Message.js
@@ -10,7 +10,7 @@ function Message() {
       <p>
         Bello bb, we are 3!
         <br />
-		I am deeply grateful for the love and care you provided me<br /> for these 3 years. Without you, I don't think I would be able<br /> to survive this harsh early adulting period. So, thank you<br /> bb!
+		I am deeply grateful for the love and care you provided<br /> me for these 3 years. Without you, I don't think I would<br /> be able to survive this harsh early adulting period. So,<br /> thank you bb!
       </p>
     </div>
   );
